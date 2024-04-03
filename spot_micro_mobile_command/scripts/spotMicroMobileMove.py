8c9e58b8bd89393f410ace641fac2c3bc1f5dd39
SamuelVoda <91326606+SamuelVoda@users.noreply.github.com>
2024-04-03 09:37:03 +0200

    Update spotMicroMobileMove.py
    added middleWare to allow localhost address for api

--- spot_micro_mobile_command/scripts/spotMicroMobileMove.py
+++ spot_micro_mobile_command/scripts/spotMicroMobileMove.py
@@ -5,9 +5,23 @@ import rospy
 from fastapi import FastAPI
 from geometry_msgs.msg import Vector3, Twist
 from std_msgs.msg import Float32MultiArray
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
